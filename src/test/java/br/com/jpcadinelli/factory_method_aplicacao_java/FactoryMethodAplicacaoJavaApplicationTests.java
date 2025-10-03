be1f9a8edd5378d787c9d66594cb098a9cee6ff6
jpcadinelli <jpcadinelli@gmail.com>
2025-10-03 18:18:51 -0300

    feat: first commit

---
+++ src/test/java/br/com/jpcadinelli/factory_method_aplicacao_java/FactoryMethodAplicacaoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jpcadinelli.factory_method_aplicacao_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactoryMethodAplicacaoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
